d195e317a2bd967dc04d828adef7a2d41ac9a8a3
uppi <helendile@gmail.com>
2014-06-15 20:38:21 +0400

    Implemented simple cases for Windows

--- printing_tools.py
+++ printing_tools.py
@@ -1,6 +1,11 @@
 import subprocess, os.path, sys, itertools
 
 IS_UNIT_TEST = False
+IS_WIN = sys.platform.lower().startswith('win')
+
+if IS_WIN:
+    import win32print
+    import win32api
 
 class PrintError(Exception):
     def __init__(self, msg):
@@ -10,53 +15,81 @@ class PrintError(Exception):
         return repr(self.msg)
 
 class WinPrinter(object):
-    def __init__(self):
-        import win32print
-
-    def list_printers(self):
-        raise NotImplementedError()
-
-    def print_file(self, filename, printer_name=None):
-        raise NotImplementedError()
-
-    def print_text(self, text, printer_name=None):
-        raise NotImplementedError()
-
-class UnixPrinter(object):
-    def list_printers(self):
-        lpstat_results = subprocess.check_output(['lpstat','-p'], universal_newlines=True)
-        return [line.split()[1] for line in lpstat_results.split('\n') if line.startswith("printer")]
+    def printers(self):
+        return [name for (_, _, name, _) in win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL)]
 
     def print_file(self, filename, printer_name=None):
         try:
             with open(filename):
                 pass
         except (OSError, IOError) as err:
-            raise PrintError("Error: %s"%str(err))
+            raise PrintError('Error: %s' % str(err))
+        exec_args = [0, 'print', filename]
+        if not printer_name:
+            printer_name = win32print.GetDefaultPrinter()
+        exec_args.append('/d:"%s"' % printer_name)
+        exec_args += ['.', 0]
+        if IS_UNIT_TEST:
+            return exec_args
+        win32api.ShellExecute(*exec_args)
+
+    def print_text(self, text, printer_name=None):
+        if not printer_name:
+            printer_name = win32print.GetDefaultPrinter()
+        try:
+            printer = win32print.OpenPrinter(printer_name)
+            try:
+                job = win32print.StartDocPrinter(printer, 1, ('PrintText', None, 'RAW'))
+                try:
+                    win32print.StartPagePrinter(printer)
+                    if not IS_UNIT_TEST:
+                        win32print.WritePrinter(printer, text)
+                    win32print.EndPagePrinter(printer)
+                finally:
+                    win32print.EndDocPrinter(printer)
+            finally:
+                win32print.ClosePrinter(printer)
+        except Exception as err:
+            print str(err)
+            if IS_UNIT_TEST:
+                return False
+            else:
+                raise PrintError(str(err))
+        return True
+
+class UnixPrinter(object):
+    def printers(self):
+        lpstat_results = subprocess.check_output(['lpstat','-p'], universal_newlines=True)
+        return [line.split()[1] for line in lpstat_results.split('\n') if line.startswith('printer')]
+
+    def print_file(self, filename, printer_name=None):
+        try:
+            with open(filename):
+                pass
+        except (OSError, IOError) as err:
+            raise PrintError('Error: %s' % str(err))
         popen_args = ['lpr']
         if printer_name:
-            popen_args.append('-P%s'%printer_name)
+            popen_args.append('-P%s' % printer_name)
         popen_args.append(filename)
         if IS_UNIT_TEST:
             return popen_args
-        else:
-            popen = subprocess.Popen(popen_args, stdin=subprocess.PIPE)
-            popen.communicate(text)
-            p.stdin.close()
+        popen = subprocess.Popen(popen_args, stdin=subprocess.PIPE)
+        popen.communicate(text)
+        p.stdin.close()
         
     def print_text(self, text, printer_name=None):
         popen_args = ['lpr']
         if printer_name:
-            popen_args.append('-P%s'%printer_name)
+            popen_args.append('-P%s' % printer_name)
         if IS_UNIT_TEST:
             return popen_args
-        else:
-            popen = subprocess.Popen(popen_args, stdin=subprocess.PIPE)
-            popen.communicate(text)
-            p.stdin.close()
+        popen = subprocess.Popen(popen_args, stdin=subprocess.PIPE)
+        popen.communicate(text)
+        p.stdin.close()
 
 print_engine = None
-if sys.platform.lower().startswith('win'):
+if IS_WIN:
     print_engine = WinPrinter()
 else:
     print_engine = UnixPrinter()
@@ -67,7 +100,7 @@ def print_file(filename, printer_name=None):
 def print_text(text, printer_name=None):
     return print_engine.print_text(text, printer_name)
 
-def list_printers():
-    return print_engine.list_printers()
+def printers():
+    return print_engine.printers()
 
 
--- tests.py
+++ tests.py
@@ -1,20 +1,36 @@
 import unittest, printing_tools
 printing_tools.IS_UNIT_TEST = True
 
-class TestUnix(unittest.TestCase):
-	def test_list(self):
-		printers = printing_tools.list_printers()
-		self.assertNotEqual(printers, [])
-		self.assertEqual(printers, ['Canon_MG3500_series'])
+if printing_tools.IS_WIN:
+    class TestWin(unittest.TestCase):
+        def test_list(self):
+            printers = printing_tools.printers()
+            self.assertIn('Canon MG3500 series Printer', printers)
 
-	def test_print_text(self):
-		self.assertEqual(['lpr'], printing_tools.print_text("Sample Text"))
-		self.assertEqual(['lpr', '-Pololo'], printing_tools.print_text("Sample Text", printer_name="ololo"))
+        def test_print_text(self):
+            self.assertEqual(True, printing_tools.print_text("Sample Text"))
+            self.assertEqual(False, printing_tools.print_text("Sample Text", printer_name="ololo"))
 
-	def test_print_file(self):
-		with self.assertRaises(printing_tools.PrintError):
-			printing_tools.print_file("azaza")
-		self.assertEqual(['lpr', '-Pololo', 'tests.py'], printing_tools.print_file("tests.py", printer_name="ololo"))
+        def test_print_file(self):
+            with self.assertRaises(printing_tools.PrintError):
+                printing_tools.print_file("azaza")
+            self.assertEqual([0, 'print', 'tests.py', '/d:"ololo"', '.', 0], 
+                printing_tools.print_file("tests.py", printer_name="ololo"))
+
+else:
+    class TestUnix(unittest.TestCase):
+        def test_list(self):
+            printers = printing_tools.printers()
+            self.assertIn('Canon_MG3500_series', printers)
+
+        def test_print_text(self):
+            self.assertEqual(['lpr'], printing_tools.print_text("Sample Text"))
+            self.assertEqual(['lpr', '-Pololo'], printing_tools.print_text("Sample Text", printer_name="ololo"))
+
+        def test_print_file(self):
+            with self.assertRaises(printing_tools.PrintError):
+                printing_tools.print_file("azaza")
+            self.assertEqual(['lpr', '-Pololo', 'tests.py'], printing_tools.print_file("tests.py", printer_name="ololo"))
 
 
 if __name__ == '__main__':
